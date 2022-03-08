cba7da5d3a3da1f4de736c135bd2b3835dd90b67
Kumar Rhythm Semwal <08rhythm@gmail.com>
2022-03-08 13:25:16 +0530

    another approach to solve the same problem

--- Stacks/min stack.py
+++ Stacks/min stack.py
@@ -1,5 +1,33 @@
 import sys
 class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        return
+
+    def pop(self) -> None:
+        if self.stack:
+            popped_element = self.stack.pop()
+            if self.min_stack and popped_element == self.min_stack[-1]:
+                self.min_stack.pop()
+        return
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        return -1
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        return -1
+
+class MinStack2:
 
     def __init__(self):
         self.stack = []
@@ -31,12 +59,12 @@ class MinStack:
         return -1
 
 
+
 # Your MinStack object will be instantiated and called as such:
 obj = MinStack()
-print(obj.push(-2))
 print(obj.push(0))
-print(obj.push(-3))
+print(obj.push(1))
+print(obj.push(0))
 print(obj.getMin())
 print(obj.pop())
-print(obj.top())
 print(obj.getMin())
