7c49b9a2cb7fe1f166e109100405a819abc70a4e
Kumar Rhythm Semwal <08rhythm@gmail.com>
2022-03-08 13:35:37 +0530

    new question

---
+++ Stacks/First Unique Character in a String.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+from operator import le
+
+
+class Solution:
+    #SC = O(1)
+    def firstUniqChar(self, s: str) -> int:
+        hash_map = defaultdict(int)
+
+        for i in range(len(s)):
+            hash_map[s[i]] += 1
+
+        for i in range(len(s)):
+            if hash_map[s[i]] == 1:
+                return i
+        return -1
+
+if __name__ == '__main__':
+    print(Solution().firstUniqChar('aabb'))
