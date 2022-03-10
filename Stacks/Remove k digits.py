d3121133528710d1fd8bd4e134530675c5e57841
Kumar Rhythm Semwal <08rhythm@gmail.com>
2022-03-10 15:53:55 +0530

    new approach added

--- Stacks/Remove k digits.py
+++ Stacks/Remove k digits.py
@@ -2,6 +2,29 @@ from operator import le
 
 
 class Solution:
+    # approach 1
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) == k:
+            return '0'
+            
+        stack = []
+        stack.append(num[0])
+        
+        for i in range(1, len(num)):
+            while stack and stack[-1] > num[i] and k > 0:
+                stack.pop()
+                k -= 1
+            
+            stack.append(num[i])
+        
+        while k > 0:
+            stack.pop()
+            k -= 1
+        
+        # this step would remove all the trailing zeros
+        return str(int("".join(stack)))
+
+    # appraoch 2
     def removeKdigits(self, num: str, k: int) -> str:
         if len(num) == k:
             return '0'
