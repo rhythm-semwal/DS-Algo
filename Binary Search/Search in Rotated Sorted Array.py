c0309664056c76c7072af2d2c7ac745d1bb6b1a5
Kumar Rhythm Semwal <08rhythm@gmail.com>
2022-03-13 11:03:58 +0530

    new code

---
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)-1
+
+        while low <= high:
+            mid = (low+high)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid+1
+                else:
+                    high = mid-1
+        return -1
+
+if __name__ == '__main__':
+    nums = [4,5,6,7,0,1,2]
+    target = 9
+    print(Solution().search(nums, target))
