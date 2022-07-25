5a34ca9bf840a6f9cba6bb9853dfffd012a8dd49
Kumar Rhythm Semwal <08rhythm@gmail.com>
2022-07-25 17:28:33 +0530

    Update Longest Consecutive subsequence.py

--- Arrays/Longest Consecutive subsequence.py
+++ Arrays/Longest Consecutive subsequence.py
@@ -2,6 +2,27 @@ class Solution:
     # @param A : tuple of integers
     # @return an integer
     def longestConsecutive(self, A):
+        # best approach
+        """
+        we create a set from the array and then check if the number is starting of the sequence
+        That we check if num-1 is not in set that means it's the starting of the sequence
+        """
+        num_set = set(A)
+
+        longest = 0
+
+        for n in A:
+            if n - 1 not in num_set:
+                length = 0
+                while (length + n) in num_set:
+                    length += 1
+                longest = max(longest, length)
+
+        return longest
+
+
+
+        #====================================================
         # approach 1 - sorting
         # T.C = O(N+NlogN) - worst case
         # S.C = O(1)
