52a170e87003a2584e83e842ddc51b311beea1dc
Kumar Rhythm Semwal <08rhythm@gmail.com>
2023-05-08 12:12:00 +0530

    Update Daily Temperatures.py

--- Stacks/Daily Temperatures.py
+++ Stacks/Daily Temperatures.py
@@ -1,7 +1,24 @@
 from typing import List
 
-
 class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack, output = [], [0]*len(temperatures)
+
+        for index in range(len(temperatures)-1, -1, -1):
+            while stack:
+                if temperatures[index] < temperatures[stack[-1]]:
+                    output[index] = stack[-1] - index
+                    stack.append(index)
+                    break
+                else:
+                    stack.pop()
+            if not stack:
+                stack.append(index)
+
+        return output
+
+    
+class Solution2:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack, output = [], [0]*len(temperatures)
 
@@ -14,6 +31,6 @@ class Solution:
         return output
 
 
-temperatures = [50,60,70]
+temperatures = [73,74,75,71,69,72,76,73]
 print(Solution().dailyTemperatures(temperatures))
 
