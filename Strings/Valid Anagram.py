cf3febbde8219a96c232efe43067ccb1f39fcce3
rhythm-s <08rhythm@gmail.com>
2023-05-09 18:27:59 +0530

    codes

--- Strings/Valid Anagram.py
+++ Strings/Valid Anagram.py
@@ -1,4 +1,18 @@
 class Solution:
+    def isAnagram3(self, s: str, t: str) -> bool:
+        count = [0] * 26
+
+        for i in s:
+            count[ord(i) - ord('a')] += 1
+
+        for i in t:
+            count[ord(i) - ord('a')] -= 1
+
+        for each in count:
+            if each != 0:
+                return False
+        return True
+
     def isAnagram1(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
