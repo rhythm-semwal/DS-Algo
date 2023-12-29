993ea8b4092b3378663f360f809361846a8cf4da
Kumar Rhythm Semwal <39095551+rhythm-semwal@users.noreply.github.com>
2023-12-29 11:58:21 +0530

    added an optimised approach using minheap

--- Arrays/kth largest element.py
+++ Arrays/kth largest element.py
@@ -30,25 +30,52 @@ class Solution2:
     @param nums: An array
     @return: the Kth largest element
     """
-    # TC = O(n + kLogn)
+    # TC = O(nlgn + klgn)
     def kthLargestElement(self, k, nums):
         # write your code here
+        if not nums or not k or k < 0:
+            return None
 
-        from heapq import heapify, heappush, heappop
+        from heapq import heappush, heappop
         max_heap = []
-        heapify(max_heap)
 
         for i in nums:
             heappush(max_heap, i*-1)
-            heapify(max_heap)
 
         while k > 1:
             heappop(max_heap)
-            heapify(max_heap)
             k -= 1
 
         return max_heap[0]*-1
 
-k = 3
-nums = [9,3,2,4,8]
-print(Solution1().kthLargestElement(k, nums))
+
+# minheap, add k element to minheap, if nums[i] > minheap[0], then pop and add it, else pass.
+#  Time: O(nlgk) Space: O(k) Runtime: 78%
+class Solution3:
+    """
+    @param k: An integer
+    @param nums: An array
+    @return: the Kth largest element
+    """
+    def kthLargestElement(self, k, nums):
+        if not nums or not k or k < 0:
+            return None
+
+        from heapq import heappush, heappop
+
+        minheap = []
+        for num in nums:
+            if len(minheap) < k:
+                heappush(minheap, num)
+            else:
+                if num > minheap[0]:
+                    heappop(minheap)
+                    heappush(minheap, num)
+
+        return minheap[0]
+
+
+k = 2
+nums = [3,2,1,5,6,4]
+# nums = [-10, -40, -25, -12, -25, -10]
+print(Solution3().kthLargestElement(k, nums))
