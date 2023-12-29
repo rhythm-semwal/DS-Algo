54524d71c92a3f888d3d36588dca484677c44461
rhythm-s <116891387+rhythm-s@users.noreply.github.com>
2023-12-29 12:17:56 +0530

    new questions solved on arrays

---
+++ Arrays/Find the Middle Index in Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMiddleIndex(self, nums) -> int:
+        left_sum, right_sum = 0, sum(nums)
+
+        for index, value in enumerate(nums):
+            right_sum -= value
+
+            if left_sum == right_sum:
+                return index
+
+            left_sum += value
+
+        return -1
