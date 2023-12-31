c6231b5d3f11a38167205d2bbe4c917bd72c5444
rhythm-s <116891387+rhythm-s@users.noreply.github.com>
2023-12-31 10:06:41 +0530

    Update vertical order traversal.py

--- Trees/vertical order traversal.py
+++ Trees/vertical order traversal.py
@@ -1,4 +1,7 @@
 # Definition for a  binary tree node
+from collections import defaultdict
+
+
 class Node:
     def __init__(self, x):
         self.val = x
@@ -6,39 +9,81 @@ class Node:
         self.right = None
 
 
-class Solution:
-    # @param A : root node of tree
-    # @return a list of list of integers
-    # approach 1
-    # def verticalOrderTraversal(self, A):
-    #     if A is None:
-    #         return []
-    #
-    #     queue = []
-    #     queue.append((A, 0))
-    #
-    #     hash_map = dict()
-    #
-    #     while queue:
-    #         temp_queue = []
-    #         for node, dist in queue:
-    #             if dist in hash_map:
-    #                 hash_map[dist].append(node.val)
-    #             else:
-    #                 hash_map[dist] = [node.val]
-    #             if node.left:
-    #                 temp_queue.append((node.left, dist-1))
-    #
-    #             if node.right:
-    #                 temp_queue.append((node.right, dist+1))
-    #
-    #         queue = temp_queue
-    #
-    #     print(hash_map)
-    #
-    #     print([hash_map[x] for x in sorted(hash_map.keys())])
+"""
+leet-ocde solution for this use case - There may be multiple nodes in the same row and same column. 
+In such a case, sort these nodes by their values.
+"""
 
-    # approach if nodes on the same vertical level order is not important
+
+class Solution1:
+    def verticalTraversal(self, root):
+        if root is None:
+            return root
+
+        hash_map = defaultdict(list)
+        queue = [(root, 0, 0)]
+
+        while queue:
+            node, pos, depth = queue.pop(0)
+
+            hash_map[(pos, depth)].append(node.val)
+            hash_map[(pos, depth)].sort()
+
+            if node.left:
+                queue.append((node.left, pos - 1, depth + 1))
+            if node.right:
+                queue.append((node.right, pos + 1, depth + 1))
+
+        res = defaultdict(list)
+        keys = sorted(list(hash_map.keys()), key=lambda x: (x[0], x[1]))
+
+        for k in keys:
+            pos, depth = k
+            res[pos].extend(hash_map[k])
+        return res.values()
+
+
+"""
+approach if nodes on the same vertical level order is not important
+"""
+
+
+class Solution2:
+    def verticalOrderTraversal(self, A):
+        if A is None:
+            return []
+
+        queue = []
+        queue.append((A, 0))
+
+        hash_map = dict()
+
+        while queue:
+            temp_queue = []
+            for node, dist in queue:
+                if dist in hash_map:
+                    hash_map[dist].append(node.val)
+                else:
+                    hash_map[dist] = [node.val]
+                if node.left:
+                    temp_queue.append((node.left, dist-1))
+
+                if node.right:
+                    temp_queue.append((node.right, dist+1))
+
+            queue = temp_queue
+
+        print(hash_map)
+
+        print([hash_map[x] for x in sorted(hash_map.keys())])
+
+
+"""
+approach if nodes on the same vertical level order is important i.e the increasing vertical order
+"""
+
+
+class Solution3:
     def __init__(self):
         import sys
         self.min_value = sys.maxsize
@@ -64,53 +109,13 @@ class Solution:
             self.min_value = min(self.min_value, level)
 
             if node.left:
-                queue.append((node.left, level-1))
+                queue.append((node.left, level - 1))
             if node.right:
-                queue.append((node.right, level+1))
+                queue.append((node.right, level + 1))
 
-        print(hash_map)
         result = []
-        print(self.min_value)
-        print(self.max_value)
-        for i in range(self.min_value, self.max_value+1):
+
+        for i in range(self.min_value, self.max_value + 1):
             result.append(hash_map[i])
 
-        print("'*****=", result)
-
-    def level_order_traversal(self, root):
-        if root is None:
-            return None
-
-        result = []
-        queue = []
-        queue.append(root)
-        while queue:
-            level = []
-            for i in range(len(queue)):
-                node = queue.pop(0)
-                level.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            result.append(level)
-
-        print(result)
-
-
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
-root.right.left.right = Node(8)
-root.right.right.left = Node(10)
-root.right.right.right = Node(9)
-root.right.right.left.right = Node(11)
-root.right.right.left.right.right = Node(12)
-print(Solution().level_order_traversal(root))
-print(Solution().verticalOrderTraversal(root))
+        return result
