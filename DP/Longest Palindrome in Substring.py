d6dfaf98beeb076898ab2dd406623d2780c67dde
rhythm-s <08rhythm@gmail.com>
2024-01-08 09:31:12 +0530

    Update Longest Palindrome in Substring.py

--- DP/Longest Palindrome in Substring.py
+++ DP/Longest Palindrome in Substring.py
@@ -1,96 +1,56 @@
-# code
-# appraoch 1
+
 # T.C = O(N**2)
+# S.C = O(N**2)
+
+class Solution1:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+
+        Max_Len = 1
+        Max_Str = s[0]
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            """
+            This condition (i - j <= 2) has been added to check the below condition
+            If the substring has a length of 1 or 2, then it's a palindrome. 
+            This is because a single character is always a palindrome, 
+            and a two-character substring is a palindrome if the characters are the same.
+           """
+            """
+            dp[j + 1][i - 1] - when I am at position i and j , then I am checking if 
+            string between j+1 and i-1 is palindrome or not
+            """
+            for j in range(i):
+                if s[j] == s[i] and (i - j <= 2 or dp[j + 1][i - 1]):
+                    dp[j][i] = True
+                    if i - j + 1 > Max_Len:
+                        Max_Len = i - j + 1
+                        Max_Str = s[j:i + 1]
+        return Max_Str
 
 
-def get_substring(word, start, end):
-    result = ""
-    for i in range(start, end):
-        result += word[i]
-
-    return result
-
-
-def longestPalindrome(s):
-    n = len(s)
-
-    lookup_table = [[0 for _ in range(n)] for _ in range(n)]
-
-    max_length = 1
-    start = 0
-    # base case for string of length 1 are palindrome so filling the diagonal values as 1
-    for i in range(n):
-        lookup_table[i][i] = 1
-
-    # checking for string of length 2
-    flag = 0
-    for i in range(n-1):
-        if s[i] == s[i+1]:
-            lookup_table[i][i+1] = 1
-            if not flag:
-                start = i
-                max_length = 2
-                flag = 1
-
-    k = 3
-    while k <= n:
-        i = 0
-        while i < (n-k+1):
-            j = i + k -1
-
-            if s[i] == s[j] and lookup_table[i+1][j-1]:
-                lookup_table[i][j] = 1
-
-                if k > max_length:
-                    start = i
-                    max_length = k
-
-            i += 1
-
-        k += 1
-
-    print(max_length)
-    return get_substring(s, start, start+max_length)
-
-input = "kjqlrzzfmlvyoshiktodnsjjp"
-print(longestPalindrome(input))
-
-# appraoch 2
 # T.C = O(N**3)
-# def is_palindrome(word, start, end):
-#     while start < end:
-#         if word[start] != word[end]:
-#             return False
-#
-#         start += 1
-#         end -= 1
-#     return True
-#
-# def get_substring(word, start, end):
-#     result = ""
-#     for i in range(start, end):
-#         result += word[i]
-#
-#     return result
-#
-# def longestPalindrome(s):
-#     max_length = 1
-#     start = 0
-#     n = len(s)
-#     for i in range(n):
-#         for j in range(i + 1, n):
-#             if is_palindrome(s, i, j):
-#                 if j - i + 1 > max_length:
-#                     max_length = j - i + 1
-#                     start = i
-#
-#     result = get_substring(s, start, start+max_length)
-#     print(result)
+# S.C = O(1)
 
-# input = "forgeeksskeegfor"
-# print(longestPalindrome(input))
-# if __name__ == '__main__':
-#     t = int(input())
-#     for i in range(t):
-#         s = input()
-#         print(longestPalindrome(s))
+class Solution2:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+
+        max_length = 0
+        max_substring = s[0]
+
+        for i in range(len(s)-1):
+            for j in range(i+1, len(s)):
+                if j-i+1 > max_length and s[i:j+1] == s[i:j+1][::-1]:
+                    max_length = j-i+1
+                    max_substring = s[i:j+1]
+
+        return max_substring
+
+
+s = 'babad'
+s = 'forgeeksskeegfor'
+print(Solution1().longestPalindrome(s))
